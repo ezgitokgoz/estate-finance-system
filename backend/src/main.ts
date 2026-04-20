485f455543892a6076456007a98e0d087361e11e
Ezgi Tokgoz <ezgitokgoz16@gmail.com>
2026-04-20 18:35:57 +0300

    update URL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://ert-estate-finance-system.netlify.app/'],
+    origin: ['http://localhost:3001', 'https://ert-estate-finance-system.netlify.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });

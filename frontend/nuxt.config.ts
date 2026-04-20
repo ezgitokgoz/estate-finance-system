040b0c2250a86dad2f3806e2202060ce2bc5f396
Ezgi Tokgoz <ezgitokgoz16@gmail.com>
2026-04-20 17:26:29 +0300

    fix: nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,13 +3,27 @@ export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: false },
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
+  
   typescript: {
     typeCheck: false,
-    shim: false
+    shim: false,
+    tsConfig: {
+      compilerOptions: {
+        skipLibCheck: true,
+      }
+    }
   },
+
+  vite: {
+    build: {
+      sourcemap: false,
+    }
+  },
+
   devServer: {
     port: 3001
   },
+
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3000'
